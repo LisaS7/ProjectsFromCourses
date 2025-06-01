757cc37425aea5706bed5e37a619382b87ef36b8
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-01 15:04:44 +0100

    Delay for scene reload

--- SnowBoarder/Assets/Sprites/Scripts/CrashDetector.cs
+++ SnowBoarder/Assets/Sprites/Scripts/CrashDetector.cs
@@ -3,12 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 1.5f;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ground")
         {
             Debug.Log("Shit!");
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
