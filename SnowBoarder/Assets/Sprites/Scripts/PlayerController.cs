1ede66b96b1cbad9a7c3503e97036e511469b553
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-01 19:12:43 +0100

    Adding boost speed control

--- SnowBoarder/Assets/Sprites/Scripts/PlayerController.cs
+++ SnowBoarder/Assets/Sprites/Scripts/PlayerController.cs
@@ -1,23 +1,49 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 0.6f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
     Rigidbody2D rb2D;
-    
+    SurfaceEffector2D surface;
+
     void Start()
     {
-        rb2D =  GetComponent<Rigidbody2D>();
+        rb2D = GetComponent<Rigidbody2D>();
+        surface = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
 
     void Update()
     {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            surface.speed = boostSpeed;
+        } else {
+            surface.speed = baseSpeed;
+        }
+        
+        
+    }
+
+    void RotatePlayer()
+    {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb2D.AddTorque(torqueAmount);
-        } else if (Input.GetKey(KeyCode.RightArrow)) {
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
             rb2D.AddTorque(-torqueAmount);
         }
     }
+
 }
