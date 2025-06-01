a78f4010baa08f2db3ac95781fba151f2569e572
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-01 16:24:51 +0100

    Hooking up finish effect and snow crash effect

--- SnowBoarder/Assets/Sprites/Scripts/CrashDetector.cs
+++ SnowBoarder/Assets/Sprites/Scripts/CrashDetector.cs
@@ -5,16 +5,18 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float reloadDelay = 0.5f;
     [SerializeField] GameObject crashBubble;
+    [SerializeField] ParticleSystem crashSnowEffect;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Ground")
         {
-            if (crashBubble != null)
+            if (crashBubble != null && crashSnowEffect != null)
             {
+                crashSnowEffect.Play();
                 crashBubble.SetActive(true);
             }
-            
+
             Invoke("ReloadScene", reloadDelay);
         }
     }
--- SnowBoarder/Assets/Sprites/Scripts/FinishLine.cs
+++ SnowBoarder/Assets/Sprites/Scripts/FinishLine.cs
@@ -4,11 +4,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float finishDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
         {
+            finishEffect.Play();
             Invoke("ReloadScene",finishDelay);
         }
 
