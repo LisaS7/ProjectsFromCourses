98545df512e5eb7d8b46c3b9f9a1b850bfd24ab1
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-03 09:35:31 +0100

    Adding fonts and Question objects

---
+++ Quiz/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "Enter new question text";
+    [SerializeField] string[] answers = new string[4];
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+}
