7cdc725f605e5e06a9f2012d0e1202393cffc482
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-03 11:07:22 +0100

    Adding correct answer functionality

--- Quiz/Assets/Scripts/Quiz.cs
+++ Quiz/Assets/Scripts/Quiz.cs
@@ -1,20 +1,34 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
     {
         questionText.text = question.GetQuestion();
 
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < answerButtons.Length; i++)
         {
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = question.GetAnswer(i); 
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct";
+            Image buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
         }
     }
 
