9388bfdeddfaecf672217113a200c1164dfe609d
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-03 12:33:47 +0100

    Disabling buttons on answer selected

--- Quiz/Assets/Scripts/Quiz.cs
+++ Quiz/Assets/Scripts/Quiz.cs
@@ -7,11 +7,41 @@ public class Quiz : MonoBehaviour
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
-    int correctAnswerIndex;
     [SerializeField] Sprite defaultAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
+    {
+        DisplayQuestion();
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        int correctAnswerIndex = question.GetCorrectAnswerIndex();
+        Image buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+        buttonImage.sprite = correctAnswerSprite;
+
+        if (index == correctAnswerIndex)
+        {
+            questionText.text = "Correct";
+        }
+        else
+        {
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = $"Nope. The correct answer is \n{correctAnswer}";
+        }
+
+        setButtonState(false);
+    }
+
+
+    void GetNextQuestion()
+    {
+        setButtonState(true);
+        DisplayQuestion();
+    }
+
+    void DisplayQuestion()
     {
         questionText.text = question.GetQuestion();
 
@@ -20,17 +50,15 @@ public class Quiz : MonoBehaviour
             TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
             buttonText.text = question.GetAnswer(i);
         }
+
     }
 
-    public void OnAnswerSelected(int index)
+    void setButtonState(bool state)
     {
-        if (index == question.GetCorrectAnswerIndex())
+        for (int i = 0; i < answerButtons.Length; i++)
         {
-            questionText.text = "Correct";
-            Image buttonImage = answerButtons[index].GetComponent<Image>();
-            buttonImage.sprite = correctAnswerSprite;
+            Button button = answerButtons[1].GetComponent<Button>();
+            button.interactable = state;
         }
     }
-
-
 }
