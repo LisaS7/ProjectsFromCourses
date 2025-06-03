cbda30888daeb4147453b4df5b9a524e53d3fd28
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-03 09:38:32 +0100

    Adding correct answers

--- Quiz/Assets/Scripts/QuestionSO.cs
+++ Quiz/Assets/Scripts/QuestionSO.cs
@@ -6,6 +6,7 @@ public class QuestionSO : ScriptableObject
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter new question text";
     [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
 
     public string GetQuestion()
     {
