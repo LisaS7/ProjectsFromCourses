ed32b02d3d92de4bfda2aafda439cc8720fb4a05
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-05 16:04:57 +0100

    Adding running animation to player controls

--- Cavernfall/Assets/Scripts/PlayerMovement.cs
+++ Cavernfall/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,20 @@ public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
     Rigidbody2D playerRB;
+    Animator animator;
     [SerializeField] float runSpeed = 5f;
 
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
 
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
@@ -26,8 +29,22 @@ public class PlayerMovement : MonoBehaviour
 
     void Run()
     {
-        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, playerRB.linearVelocity.y);
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, playerRB.linearVelocityY);
         playerRB.linearVelocity = playerVelocity;
+
+        bool hasHorizontalSpeed = Mathf.Abs(playerRB.linearVelocityX) > Mathf.Epsilon;
+        animator.SetBool("isRunning", hasHorizontalSpeed);
+
     }
 
+    void FlipSprite()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(playerRB.linearVelocityX) > Mathf.Epsilon;
+        if (hasHorizontalSpeed)
+        {
+
+            transform.localScale = new Vector2(Mathf.Sign(playerRB.linearVelocityX), 1f);
+        }
+
+    }
 }
