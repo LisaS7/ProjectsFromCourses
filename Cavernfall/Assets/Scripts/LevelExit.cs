e4e4246a18ab739e63fd48f84a2326e9430f3245
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-06 17:14:43 +0100

    Add exits to levels and load next level functionality

---
+++ Cavernfall/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
