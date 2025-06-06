ea00f044f478f5691dfb8224bca5aa611d8bfd50
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-06 12:04:42 +0100

    Adding enemy sprite with movement

---
+++ Cavernfall/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Rigidbody2D enemyRB;
+
+    [SerializeField] float moveSpeed = 1f;
+
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        enemyRB.linearVelocity = new Vector2(moveSpeed, 0);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemy();
+    }
+
+    void FlipEnemy()
+    {
+        transform.localScale = new Vector2(-Mathf.Sign(enemyRB.linearVelocityX), 1f);
+    }
+}
