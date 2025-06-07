abbd9f01f59d94f5a79ff1742fda16f698321f3a
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-07 19:23:05 +0100

    Adding basic player movement

--- BugBlaster/Assets/Scripts/Player.cs
+++ BugBlaster/Assets/Scripts/Player.cs
@@ -1,15 +1,33 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    Animator animator;
+    Vector2 rawInput;
+    [SerializeField] float moveSpeed = 5f;
 
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
 
+        Move();
+    }
+
+    void Move()
+    {
+        bool isMoving = rawInput != Vector2.zero;
+        animator.SetBool("isMoving", isMoving);
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
     }
 }
