a66bb0443fd1170ccf2981a94a7ca230bd4989bc
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-07 21:23:09 +0100

    Setting up paths and waves

---
+++ BugBlaster/Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+    [SerializeField] Transform pathPrefab;
+    [SerializeField] float moveSpeed = 5f;
+
+    public float GetMoveSpeed()
+    {
+        return moveSpeed;
+    }
+
+    public Transform GetStartWaypoint()
+    {
+        return pathPrefab.GetChild(0);
+    }
+
+    public List<Transform> GetWaypoints()
+    {
+        List<Transform> waypoints = new();
+        foreach (Transform waypoint in pathPrefab)
+        {
+            waypoints.Add(waypoint);
+        }
+        return waypoints;
+    }
+
+}
