579c1f72848b79cf0a9687b085407ce90acd3110
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-21 21:38:09 +0100

    Adding player movement

--- UdemyRPG/Assets/Scripts/PlayerController.cs
+++ UdemyRPG/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float moveSpeed;
 
     void Start()
     {
@@ -12,6 +13,6 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
+        rb.linearVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
     }
 }
