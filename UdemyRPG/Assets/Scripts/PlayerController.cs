935a8c1c57a399145d4ca43bc6c76a38b8a74fdc
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-22 11:49:36 +0100

    Connecting walking and idle animations to movement

--- UdemyRPG/Assets/Scripts/PlayerController.cs
+++ UdemyRPG/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,25 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public float moveSpeed;
 
+    Animator animator;
+
     void Start()
     {
-
+        animator = GetComponent<Animator>();
     }
 
 
     void Update()
     {
         rb.linearVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        animator.SetFloat("moveX", rb.linearVelocityX);
+        animator.SetFloat("moveY", rb.linearVelocityY);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            animator.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
     }
 }
