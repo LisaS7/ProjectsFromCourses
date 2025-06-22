c96432425d53fc35b6693fc28cf370630ae9c1a6
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-22 20:47:29 +0100

    Adding player loader

---
+++ UdemyRPG/Assets/Scripts/PlayerLoader.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerLoader : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        if (PlayerController.instance == null)
+        {
+            Instantiate(player);
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+}
