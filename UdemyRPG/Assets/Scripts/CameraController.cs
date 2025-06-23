d64b10f80aff7323b76198da9d33e3df76645ed9
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-23 19:59:43 +0100

    Adding limits to camera movement

--- UdemyRPG/Assets/Scripts/CameraController.cs
+++ UdemyRPG/Assets/Scripts/CameraController.cs
@@ -1,17 +1,30 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public Tilemap map;
+    Vector3 bottomLeftLimit;
+    Vector3 topRightLimit;
 
     void Start()
     {
         target = PlayerController.instance.transform;
+
+        bottomLeftLimit = map.localBounds.min;
+        topRightLimit = map.localBounds.max;
     }
 
 
     void LateUpdate()
     {
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+        // keep the camera inside the map
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x),
+            Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y),
+            transform.position.z);
     }
 }
