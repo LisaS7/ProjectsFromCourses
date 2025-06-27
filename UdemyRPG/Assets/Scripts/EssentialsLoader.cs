6e211fa2a6ab98c4a68079362e515e5a2452e5e6
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-27 22:04:26 +0100

    Adding Game Manager to Essentials Loader

--- UdemyRPG/Assets/Scripts/EssentialsLoader.cs
+++ UdemyRPG/Assets/Scripts/EssentialsLoader.cs
@@ -4,6 +4,7 @@ public class EssentialsLoader : MonoBehaviour
 {
     public GameObject UICanvas;
     public GameObject player;
+    public GameObject gManager;
 
     void Start()
     {
@@ -17,6 +18,11 @@ public class EssentialsLoader : MonoBehaviour
         {
             PlayerController clone = Instantiate(player).GetComponent<PlayerController>();
             PlayerController.instance = clone;
+        }
+
+        if (GameManager.instance == null)
+        {
+            Instantiate(gManager);
         }
     }
 
