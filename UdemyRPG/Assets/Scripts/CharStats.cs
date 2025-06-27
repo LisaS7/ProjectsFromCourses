337703a4a21c67ace5937dc4f1feeb0430bec297
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-27 17:30:08 +0100

    Setting up simple stats system

---
+++ UdemyRPG/Assets/Scripts/CharStats.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+    public string charName;
+    public int playerLevel = 1;
+    public int currentExp;
+
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMp = 100;
+    public int strength;
+    public int defence;
+    public int weaponPower;
+    public int armourPower;
+    public string equippedweapon;
+    public string equippedArmour;
+    public Sprite charImage;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
