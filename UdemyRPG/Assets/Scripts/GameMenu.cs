b391eb9cc6770fc52c9866288237fbfbddd4b795
Lisa <84908214+LisaS7@users.noreply.github.com>
2025-06-28 22:22:08 +0100

    Populating player stats

--- UdemyRPG/Assets/Scripts/GameMenu.cs
+++ UdemyRPG/Assets/Scripts/GameMenu.cs
@@ -1,8 +1,16 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject menuPanel;
+    private CharStats[] playerStats;
+    public TextMeshProUGUI[] nameText, HPText, MPText, levelText, expText;
+    public Slider[] expSlider;
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
+
 
     void Start()
     {
@@ -21,7 +29,33 @@ public class GameMenu : MonoBehaviour
             else
             {
                 menuPanel.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.menuActive = true;
+            }
+        }
+    }
+
+    public void UpdateMainStats()
+    {
+        playerStats = GameManager.instance.playerStats;
+
+        for (int i = 0; i < playerStats.Length; i++)
+        {
+            if (playerStats[i].gameObject.activeInHierarchy)
+            {
+                charStatHolder[i].SetActive(true);
+                nameText[i].text = playerStats[i].charName;
+                HPText[i].text = $"HP: {playerStats[i].currentExp}/{playerStats[i].maxHP}";
+                MPText[i].text = $"MP: {playerStats[i].currentMP}/{playerStats[i].maxMP}";
+                levelText[i].text = $"Level: {playerStats[i].playerLevel}";
+                expText[i].text = $"{playerStats[i].currentExp}/{playerStats[i].expToNextLevel[playerStats[i].playerLevel]}";
+                expSlider[i].maxValue = playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+                expSlider[i].value = playerStats[i].currentExp;
+                charImage[i].sprite = playerStats[i].charImage;
+            }
+            else
+            {
+                charStatHolder[i].SetActive(false);
             }
         }
     }
